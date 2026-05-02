74749f4b7641392eaed98a68e23f682ec7289dfc
Luna Alexander <auramalexander@gmail.com>
2026-05-02 13:10:21 -0500

    added extras folder

---
+++ extra/custom-body.blade.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="{{themeAsset('your.css')}}">
+
+<style> /* This is a comment */ </style>
+
+<script> // This is a comment </script>
+
+{{-- This is a comment --}}
+
+<?php // This is a comment ?>
